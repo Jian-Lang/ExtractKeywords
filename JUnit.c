9ba86f7641f5e1e981d95135d21e8895df565337
Lobster <Lobster_Jian_Lang@163.com>
2021-09-23 11:48:23 +0800

    Test Code Push:This is the c file (test code) for JUnit test

---
+++ JUnit.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+	int i = 1;
+	double j = 0;
+	if(a>b){
+		if(a>b){
+
+		}else{
+			double b;
+		}
+	}else{
+		if(a>b){
+			switch i:
+			case 1:
+			break;
+			case 2;
+			break;
+			default:
+			break;
+		}else if(a<b){
+
+		}else if(a==b){
+			int a;
+		}
+	}
+
+}
